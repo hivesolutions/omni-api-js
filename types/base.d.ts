19983bba0eafe068f19acca1ddbf171b023a41e9
João Magalhães <joamag@gmail.com>
2023-02-06 07:12:57 +0000

    fix: better types definition

--- types/base.d.ts
+++ types/base.d.ts
@@ -3,8 +3,10 @@ import { API as BaseAPI } from "yonius";
 import { UserAPI } from "./user";
 
 interface APIInterface extends UserAPI {
-    constructor(kwargs?: object);
     ping(): Promise<object>;
 }
 
-export declare class API extends BaseAPI implements APIInterface {}
+export declare class API extends BaseAPI implements APIInterface {
+    ping(): Promise<object>;
+    selfUser(options: object): Promise<object>;
+}
